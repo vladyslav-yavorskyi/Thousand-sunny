77e7f514d629d637322390913a05739b557afb94
Vladyslav <vlad09vlad09@gmail.com>
2020-11-02 10:18:02 +0100

    added 4 section

--- dist/scripts.bcf3243b.js
+++ dist/scripts.bcf3243b.js
@@ -236,7 +236,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57900" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58109" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
